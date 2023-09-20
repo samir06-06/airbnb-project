7059ee185fb3a1016536c1cf64729641dd54a94f
samir06-06 <rehimov.samir060@gmail.com>
2023-09-20 19:57:38 +0400

    Inital Commit

---
+++ src/data/data.js
@@ -0,0 +1,106 @@
+export default [
+  {
+      id: 1,
+      title: "Life Lessons with Katie Zaferes",
+      description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+      price: 136,
+      coverImg: "Dara Torres.jpg",
+      stats: {
+          rating: 5.0,
+          reviewCount: 6
+      },
+      location: "Online",
+      openSpots: 0,
+  },
+  {
+      id: 2,
+      title: "Learn Wedding Photography",
+      description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+      price: 125,
+      coverImg: "wedding.jpg",
+      stats: {
+          rating: 5.0,
+          reviewCount: 30
+      },
+      location: "Online",
+      openSpots: 27,
+  },
+  {
+    id: 3,
+    title: "Being a Chef",
+    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+    price: 136,
+    coverImg: "cook-2.jpg",
+    stats: {
+        rating: 5.0,
+        reviewCount: 6
+    },
+    location: "Online",
+    openSpots: 0,
+  },
+  {
+      id: 4,
+      title: "Group Mountain Biking",
+      description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+      price: 50,
+      coverImg: "bike.jpg",
+      stats: {
+          rating: 4.8,
+          reviewCount: 2
+      },
+      location: "Norway",
+      openSpots: 3,
+  },
+  {
+    id: 5,
+    title: "Home Design",
+    description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+    price: 125,
+    coverImg: "living-room.jpg",
+    stats: {
+        rating: 5.0,
+        reviewCount: 30
+    },
+    location: "Online",
+    openSpots: 27,
+  },
+  {
+    id: 6,
+    title: "Dance is expression of happiness",
+    description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    price: 50,
+    coverImg: "dance.png",
+    stats: {
+        rating: 4.8,
+        reviewCount: 2
+    },
+    location: "Norway",
+    openSpots: 3,
+  },
+  {
+    id: 7,
+    title: "Famous Swimmer Won 2nd place this year",
+    description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+    price: 125,
+    coverImg: "swimmer-woman-2.jpg",
+    stats: {
+        rating: 5.0,
+        reviewCount: 30
+    },
+    location: "Online",
+    openSpots: 27,
+  },
+  {
+    id: 8,
+    title: "Music gets less attention this year",
+    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+    price: 136,
+    coverImg: "musician.jpg",
+    stats: {
+        rating: 5.0,
+        reviewCount: 6
+    },
+    location: "Online",
+    openSpots: 0,
+  }
+] 
